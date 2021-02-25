4ffe76f8f79cfb78eb108d1e1cf89b37173e7f86
Leslie <leaguiar@ucsd.edu>
2021-02-25 10:47:36 -0800

    didn't finish rsvp

--- public/js/introHCI.js
+++ public/js/introHCI.js
@@ -10,8 +10,8 @@ $(document).ready(function() {
  */
 function initializePage() {
 	console.log("Page ready");
- 	// initCamera();
- 	// initMap();
+ 	initCamera();
+ 	initMap();
  	initGestures();
  	initRSVPForm();
 }
@@ -19,9 +19,34 @@ function initializePage() {
 // init jQuery gestures  
 function initGestures() {
 	// add gestures listener here
+	$(function() {
+		$(".judge-img").bind("taphold", tapholdHandler);
+
+		function tapholdHandler(event){
+			// get the id of the event source
+			var targetIDPrefix = event.target.id;
+			console.log("got prefix:" + targetIDPrefix);
+			// show bio
+			$("#" + targetIDPrefix + "-bio").show();
+		}
+	});
 }
 
 // init RSVP form submit listener
 function initRSVPForm() {
+	$('#rsvpForm').submit(function(e) {
+
+		//prevents default submit and reload 
+		e.preventDefault();
+		console.log("Submitting form...");
+		var rsvpEmail = $('#rsvpEmail').val();
+		// send the post request
+		$.post('addRSVP', { rsvpEmail: rsvpEmail }, postCallback);
+	});
+
+	function postCallback(res){
+		alert("RSVP form successfully submitted!");
+		$('#rsvpEmail').val('');
+	}
   // add your code here
 }
--- public/js/mapQuest.js
+++ public/js/mapQuest.js
@@ -1,3 +1,13 @@
 function initMap() {
 	// add your code here
+	L.mapquest.key = 'C5Kxk7ZYFNT80CjZMSkBtzircdIvTTCN';
+
+	var map = L.mapquest.map('map', {
+  	center: [32.88, -117.236],
+  	layers: L.mapquest.tileLayer('map'),
+  	zoom: 12
+	});
+
+
+	L.marker([32.87877256966731, -117.23586454456678]).addTo(map);
 }
